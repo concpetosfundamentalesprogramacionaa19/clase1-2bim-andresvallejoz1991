f0c16ed558f18e2a7bf1ea4fdd201c02c0fbbd06
andresvallejoz1991 <andresvallejoz@hotmail.com>
2019-05-30 11:23:43 -0500

    Arreglos

--- javaapplication75/src/javaapplication75/Ejemplo31.java
+++ javaapplication75/src/javaapplication75/Ejemplo31.java
@@ -5,7 +5,6 @@
  */
 package javaapplication75;
 
-
 import java.util.Scanner;
 
 /**
@@ -16,64 +15,59 @@ public class Ejemplo31 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int i = 0;//Declaracion del contador
-        
-       int[] datos = new int[6];//Declaracion del arreglo
-       boolean bandera = true;//Declaracion de bandera para el ciclo while
-        
-        String[] rangos = {"0-8", "8.1-12", "12.1-16","16.1-19","19.1-20",
+        int[] datos = new int[6];//Declaracion del arreglo
+        boolean bandera = true;//Declaracion de bandera para el ciclo while
+
+        String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19", "19.1-20",
             "20.1 en adelante"};
-        
-        while (bandera == true){//Ciclo While para ingreso de datos
-        System.out.print("Ingrese la nota\n");//Mensaje de notas
-        double cantidad = entrada.nextDouble();
+
+        while (bandera == true) {//Ciclo While para ingreso de datos
+            System.out.print("Ingrese la nota\n");//Mensaje de notas
+            double cantidad = entrada.nextDouble();
 //Entrada de los datos minetras la bandera sea True
 //Condicionales para los rangos de "0-8", "8.1-12", "12.1-16","16.1-19",
 //"19.1-20" "20.1 en adelante"
-        if(cantidad <8){
-            datos[0] = datos[0] + 1;
-        }else{
-            if(cantidad>=8.1 && cantidad <=12){
-                datos[1] = datos[1] + 1;
-            }else{
-                if (cantidad>=12.1 && cantidad <=16 ){
-                     datos[2] = datos[2] + 1;
-                }else{
-                     if (cantidad>=12.1 && cantidad <=16 ){
-                     datos[3] = datos[3] + 1;
-                     }else {
-                          if (cantidad>=16.1 && cantidad <=19 ){
-                              datos[4] = datos[4] + 1;
-                          }else {
-                              if (cantidad>=19.1 && cantidad <=20 ){
-                                  datos[5] = datos[5] + 1;
-                              }else{
-                                   if (cantidad>=20.1 ){
-                                       datos[6] = datos[6] + 1;
-                                   }
-                              }
-                          }
-                     }
+            if (cantidad <= 8) {
+                datos[0] = datos[0] + 1;
+            } else {
+                if (cantidad > 8 && cantidad <= 12) {
+                    datos[1] = datos[1] + 1;
+                } else {
+                    if (cantidad > 12 && cantidad <= 16) {
+                        datos[2] = datos[2] + 1;
+                    } else {
+                        if (cantidad > 16 && cantidad <= 19) {
+                            datos[3] = datos[3] + 1;
+                        } else {
+                            if (cantidad > 19 && cantidad <= 20) {
+                                datos[4] = datos[4] + 1;
+                            } else {
+                                if (cantidad >= 19.1 && cantidad <= 20) {
+                                    datos[5] = datos[5] + 1;
+                                } else {
+                                    datos[6] = datos[6] + 1;
+                                }
+                            }
+                        }
+                    }
                 }
-                
-                
+
             }
-        }
+
 //En caso de que el usuario decida continar ingresando datos presionara 2 
 //caso contrario con el 1 saldra del ciclo
-       System.out.println("Presione 1 para salir | Presione 2 para continuar ");
-        int opcion = entrada.nextInt();//Entrada de los datos
-        if (opcion ==1){
-            bandera = false;//bandera en falso
-        }
-        }
-        System.out.println("Notas ingresadas");//Impresion del encabezado
-        //Impresion de los datos y el rango
-        for(i = 0; i<datos.length; i++){
-            System.out.printf("%s = %d\n", rangos[i], datos[i]);
+            System.out.println("Presione 1 para salir | Presione 2 para continuar ");
+            int opcion = entrada.nextInt();//Entrada de los datos
+            if (opcion == 1) {
+                bandera = false;//bandera en falso
+            }
         }
         
-
+        System.out.println("Notas ingresadas\nRangos:");//Impresion del encabezado
+        //Impresion de los datos y el rango
+        for (int i = 0; i < datos.length; i++) {
+            System.out.printf("%s = %d\n", rangos[i], datos[i]);
+        }
 
     }
 }
